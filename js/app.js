d868e813ac519a00f24f44b501bebfd7fc10e502
jamaspy <aspin@me.com>
2019-04-02 20:17:58 +1100

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("app.JS LOADED");
+
+const options = ["rock", "paper", "scissors"]
+
+const theGame = {
+  userScore: 0,
+  computerScore: 0,
+
+  userChoice: null,
+  computerChoice: null,
+}
+
+
+##NOTES
+Rock beats Scissors
+Scissors beats Paper
+Paper beats Rock
